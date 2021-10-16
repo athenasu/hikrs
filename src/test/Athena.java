3d885a4658ff0846ced38ca64a14319f908a8ad8
Luciechen <weijuchen1992@gmail.com>
2021-10-16 16:04:04 +0800

    lulu commit

--- src/test/Athena.java
+++ src/test/Athena.java
@@ -5,6 +5,8 @@ public class Athena {
 	public static void main(String[] args) {
 		System.out.println("Hello 神隊友們");
 		System.out.println("woohoo");
+		System.out.println("Here is Lulu");
+
 	}
 
 }
