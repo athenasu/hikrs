5fd359aa98cfa72b6a707bcc4e2fd9be35f22098
Leohao0 <baby80617@hotmail.com>
2021-10-16 15:23:39 +0800

    no message

--- src/LeoTest.java
+++ src/LeoTest.java
@@ -3,7 +3,7 @@ public class LeoTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi this is Leo")
+		System.out.println("hi this is Leo");
 	}
 
 }
